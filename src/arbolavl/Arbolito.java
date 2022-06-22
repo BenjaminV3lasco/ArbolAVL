353793191e32136cdddc99039d9f214e176cfa73
Benjamín Velasco <velascobenjamin14@gmail.com>
2022-06-22 18:24:54 -0300

    Version 2

---
+++ src/arbolavl/Arbolito.java
@@ -0,0 +1,6 @@
+
+package arbolavl;
+
+public class Arbolito {
+    
+}
---
+++ src/arbolavl/NodoAvl.java
@@ -0,0 +1,16 @@
+
+package arbolavl;
+
+public class NodoAvl {
+    int dato;
+    int fe; //Factor de equilibrio
+    NodoAvl hijoIzq, hijoDcho;
+    
+    //Constructor
+    public NodoAvl (int d){
+        this.dato = d;
+        this.fe = 0;
+        this.hijoIzq=null;
+        this.hijoDcho=null;
+    }
+}

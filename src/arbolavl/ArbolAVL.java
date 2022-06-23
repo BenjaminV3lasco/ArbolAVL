7c09514e0d7f806f4d4152d7ab4ffd0c5f7ac51e
Benjamín Velasco <velascobenjamin14@gmail.com>
2022-06-23 14:21:20 -0300

    Version 4

--- src/arbolavl/ArbolAVL.java
+++ src/arbolavl/ArbolAVL.java
@@ -1,10 +1,57 @@
 
 package arbolavl;
-
+import java.util.Scanner;
 public class ArbolAVL {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int respuesta, respuesta_arbol;
+
+            //Menú
+            System.out.println("¡Bienvenido al menú para crear un Árbol Avl!");
+        do{
+            System.out.println("\nIngrese 1 para insertar un nodo");
+            System.out.println("Ingrese 2 para recorrer el árbol");
+            System.out.println("Ingrese 3 para salir");
+            respuesta=sc.nextInt();
+            sc.nextLine();
+                while(respuesta<1 || respuesta>3){
+                System.out.println("Error: La opcion ingresa no es válida");
+                System.out.println("Ingrese 1 para insertar un nodo");
+                System.out.println("Ingrese 2 para recorrer el árbol");
+                System.out.println("Ingrese 3 para salir");
+                respuesta=sc.nextInt();
+                sc.nextLine();
+                }
+            switch(respuesta){
+               case 1:
+                System.out.println("¡Los nodos fueron insertados de manera correcta!");
+                    break;
+                case 2:
+                   System.out.println("¿Como desea recorrer el árbol?");
+                   System.out.println("1. Recorrido Inorden");
+                   System.out.println("2. Recorrido Preorden");
+                   System.out.println("3. Recorrido Postorden");
+                   respuesta_arbol=sc.nextInt();
+                   sc.nextLine();
+                    switch(respuesta_arbol){
+                        case 1:
+                    System.out.println("Recorrido en Inorden:");
+                    
+                    break;
+                        case 2:
+                    System.out.println("Recorrido en Preorden:");
+                    
+                     break;
+                        case 3:
+                     System.out.println("Recorrido en Postorden:");
+                     
+                     break;
+                    }
+                    break;
+                }    
+        }while(respuesta<3);
+    }
     }
     
 }
